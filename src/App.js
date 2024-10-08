8fb20035594d6003f11c1a5bb9c426b782cdc0cf
ShachiMishra17 <shachimishra639@gmail.com>
2024-10-08 19:11:07 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    // using axios
+    // const fetchData = async () => {
+    //   try {
+    //     const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+    //     setData(response.data);
+    //     console.log(response.data);
+    //   } catch (error) {
+    //     console.error(error);
+    //   }
+    // };
+
+    // using fetch with then
+    // const fetchData =  () => {
+    //   try {
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //     .then(data => data.json())
+    //     .then(data => setData(data));
+    //   } catch (error) {
+    //     console.error(error);
+    //   }
+    // };
+
+    // using fetch with async await
+    const fetchData = async ()=>
+    {
+      try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await response.json();
+        setData(data);
+      }
+      catch(error){
+        console.error(error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  const filteredData = data.filter(user =>
+    user.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <input
+        type="text"
+        placeholder="Search by name..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <ul>
+        {filteredData.map(user => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
